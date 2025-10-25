a41fd91a24df3c53b85eb659b443860c2456c0f9
Étienne Charignon <etienne.charignon@gmail.com>
2025-10-25 11:45:54 +0200

    unfold beancount files by default

--- lua/plugins/vim-beancount.lua
+++ lua/plugins/vim-beancount.lua
@@ -1,4 +1,13 @@
 return {
   "nathangrigg/vim-beancount",
   opts = {},
+  config = function()
+    -- Désactiver le repliage par défaut pour les fichiers beancount
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "beancount",
+      callback = function()
+        vim.opt_local.foldenable = false
+      end,
+    })
+  end,
 }
